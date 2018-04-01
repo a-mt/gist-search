164d6f3f1f0c85e5a50150de08886b1d3a4daa64
Aurelie Mistri <amistri@live.fr>
2018-04-01 07:32:50 +0000

    Add credentials

--- inc.config.php
+++ inc.config.php
@@ -1,4 +1,16 @@
 <?php
-define("CLIENT_ID", "");
-define("CLIENT_SECRET", "");
-define("PASSPHRASE", "*R}@ox.i{Pu&`n\sOb5-7Ga(X$mTu^#U?C;;r~89p2Kn4O?YA");
+
+// Load .env file
+if(file_exists(__DIR__ . '/.env')) {
+  $rows = explode("\n", file_get_contents(__DIR__ . '/.env'));
+
+  foreach($rows as $row) {
+      list($k, $v) = explode("=", $row, 2);
+      define($k, trim($v, '"'));
+  }
+}
+
+// Load environement variables
+foreach($_ENV as $k => $v) {
+  define($k, $v);
+}
